52d9f78fb2e3e66d7b471723fb26c464b31eea0b
datomezz <datomezz@gmail.com>
2019-11-29 00:03:00 +0400

    add audio

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let areaCount = [0,1,2,3,4,5,6,7,8];
 let answerArr = new Array(); // used to disable random() at 9th element
 let header = document.querySelector(".header h1");
 
+const click_sound = new Audio("../audio/click.mp3");
+const botClick_sound = new Audio("../audio/botClick.mp3");
+const finish_sound = new Audio("../audio/finish.mp3");
+
+
 let switcher = 0;
 
 let player1 = new String();
@@ -23,11 +28,13 @@ for(let i = 0; i < areaArr.length; i++){
             areaArr[i].classList.add("active"); 
             areaArr[i].style.backgroundColor = "#0abde3";
             header.innerText = "X's Turn";
+            click_sound.play();
             winnerChoose("OOO");
         } else if(answerArr.length % 2 == 1){
             areaArr[i].innerText = "X";
             areaArr[i].classList.add("active");
             header.innerText = "O's Turn";
+            click_sound.play();
             winnerChoose("XXX");
         }
     }
@@ -39,6 +46,7 @@ for(let i = 0; i < areaArr.length; i++){
         areaArr[i].innerText = "X";
         areaArr[i].classList.add("active"); 
         areaCount[i] = 9;
+        click_sound.play();
         winnerChoose("XXX");
         if(answerArr.length >= 8){
             winnerChoose("XXX");
@@ -53,6 +61,7 @@ function random(){
     if(areaCount[rand] != 9){
         answerArr.push(rand);
         areaCount[rand] = 9;
+        botClick_sound.play();
         areaArr[rand].innerText = "O";
         areaArr[rand].classList.add("active"); 
         areaArr[rand].style.backgroundColor = "#0abde3";
@@ -81,7 +90,7 @@ function winnerChoose(y){
        x[2].innerText + x[4].innerText + x[6].innerText === y ){
        header.innerText = result + " - WIN";
        header.style.fontSize = "14vh";
-
+        finish_sound.play();
        for(let i = 0; i < x.length; i++){
            x[i].style.pointerEvents = "none";
        }
