7f9ca17ea2059dcaece472896128c557f2ae6ce5
datomezz <datomezz@gmail.com>
2019-11-29 17:56:10 +0400

    fixed sound problem

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ let areaCount = [0,1,2,3,4,5,6,7,8];
 let answerArr = new Array(); // used to disable random() at 9th element
 let header = document.querySelector(".header h1");
 
-const click_sound = new Audio("click.mp3");
-const botClick_sound = new Audio("botClick.mp3");
-const finish_sound = new Audio("finish.mp3");
+const click_sound = new Audio();
+const botClick_sound = new Audio();
+const finish_sound = new Audio();
+click_sound.src = "audio/click.mp3";
+botClick_sound.src = "audio/botClick.mp3";
+finish_sound.src = "finish.mp3";
 
 
 let switcher = 0;
